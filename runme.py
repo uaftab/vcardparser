66f9fe26273303873ec9f743a9be98050d8e62bc
Umair Aftab <umair.ops@gmail.com>
2017-02-26 17:00:33 -0500

    File I/O and parsing

--- runme.py
+++ runme.py
@@ -8,11 +8,11 @@ import vcard
 
 #Globals .... hisss
 vcardlist=[]
-
+spacer = "    "
 #defs
-vcard_begin = "BEGIN:VCARD"
-vcard_end    = "END:VCARD"
-
+vcard_begin = "BEGIN:VCARD\n"
+vcard_end    = "END:VCARD\n"
+linespervcardobject = 8
 
 #read from file and return as list
 def readfile(filename):
@@ -21,8 +21,6 @@ def readfile(filename):
     with open(filename,'r') as file:
         for line in file:
             filecontents.append(line)
-
-
     return filecontents
 
 def parse(filecontents):
@@ -34,18 +32,27 @@ def parse(filecontents):
             accumulate = True # Start accumulate of line into obj
         if accumulate: #Build object ? 
             obj.append(line)
-        if line == vcard_end # Are we at the end section of object
+        if line == vcard_end: # Are we at the end section of object
             accumulate = False
             vcardobjs.append(obj)
             obj = [] # reset object to empty
-    if not accumulate:
-        printf("Malformed vcard obj file missing %s",vcard_end)
+    if accumulate:
+        print("Malformed vcard obj file missing %s",vcard_end)
+        print (obj)
         sys.exit(-1)
     return vcardobjs
+
+def parsingsanitycheck(Numoflinesreadfromfile,Numofobjesctsparsed):
+    if ((Numoflinesreadfromfile/Numofobjesctsparsed) == linespervcardobject): 
+        print(spacer,"[OK] - Parsing sanity check passed. - Each vcard object is composed of",linespervcardobject)
+    else:
+        print(spacer,"[XX] - Num of objs parsed wrt to lines parsed is not whole")
+
+    return
+
+
+#Main routine
 if __name__ == "__main__":
-    contents = read('vcardfile')
+    contents = readfile('vcardfile.vcard')
     objs     = parse(contents)
-
-    print (objs)
-    print (len(objs))
-
+    #parsingsanitycheck(len(contents),len(objs))
